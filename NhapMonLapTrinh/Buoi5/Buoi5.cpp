ee0718c273ad5a21b0aa9cab8b934f23ad9a04e2
Linh Ngô <ngo.linh@daihoclongan.edu.vn>
2025-11-25 11:37:37 +0700

    Buoi 5

--- NhapMonLapTrinh/Buoi5/Buoi5.cpp
+++ NhapMonLapTrinh/Buoi5/Buoi5.cpp
@@ -1,20 +1,25 @@
-// Buoi5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*cout << "Nhap so: ";
+	int n;
+	cin >> n;
+	if (n%2 == 0) {
+		cout << "So " <<n<< " la so chan" << endl;
+	}
+	else {
+		cout << "So " << n << " la so le" << endl;
+	}*/
+
+	cout << "Nhap nam: ";
+	int year;
+	cin >> year;
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		cout << year << " la nam nhuan" << endl;
+	}
+	else {
+		cout << year << " khong phai la nam nhuan" << endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
